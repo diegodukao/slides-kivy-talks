d3fd48aca2a3e9955bbf45faa872e23c5752909f
Diego Dukão <diegodukao@gmail.com>
2016-10-27 18:04:05 -0200

    testing layout positions

---
+++ main.py
@@ -0,0 +1,17 @@
+from kivy.app import App
+from kivy.core.window import Keyboard, Window
+
+
+class Slides(App):
+
+    def on_key_down(self, window, key, *args):
+        if key == Keyboard.keycodes['right']:
+            self.root.load_next()
+        if key == Keyboard.keycodes['left']:
+            self.root.load_previous()
+
+    def on_start(self):
+        Window.bind(on_key_down=self.on_key_down)
+
+if __name__ == "__main__":
+    Slides().run()
