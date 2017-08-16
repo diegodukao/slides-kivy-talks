b26acdd57d1e1ca569d86085d183c92482c0e88f
Diego Dukão <diegodukao@gmail.com>
2017-08-15 23:55:56 -0300

    pyam exported to pdf

--- 1708-pyam/main.py
+++ 1708-pyam/main.py
@@ -3,6 +3,8 @@ from kivy.config import Config
 from kivy.core.window import Keyboard, Window
 from kivy.utils import get_color_from_hex
 
+from pdfexporter import save_screenshot
+
 
 class Slides(App):
 
@@ -11,11 +13,15 @@ class Slides(App):
             self.root.load_next()
         if key == Keyboard.keycodes['left']:
             self.root.load_previous()
+        if key == Keyboard.keycodes['down']:
+            save_screenshot(self.root.index)
 
     def on_start(self):
         Window.bind(on_key_down=self.on_key_down)
 
 if __name__ == "__main__":
     Window.clearcolor = get_color_from_hex('#FFFFFF')
+    Window.size = (1280, 960)
+    # Window.fullscreen = True
     Config.write()
     Slides().run()
